3e05ce7c1226ee1438be6a8de591d552849fe988
Julia Gois <goisjulia@ibm.com>
2020-07-28 03:17:51 -0300

    WIP Criacao dos componentes iniciais do projeto e pequenas estilizacoes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
+  let carousels = [];
+
+  for (const [index] of dadosIniciais.categorias.entries()) {
+    carousels.push(<Carousel
+      key={index}
+      ignoreFirstVideo
+      category={dadosIniciais.categorias[index]}
+    />)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Quando foi lançada como single, 'Bohemian Rhapsody' se tornou um sucesso comercial, ficando no topo da UK Singles Chart por nove semanas e vendendo mais de um milhão de cópias até o fim de janeiro de 1976. Ela alcançou o topo das listas em diversos outros mercados, incluindo Canadá, Austrália, Nova Zelândia, Irlanda e Holanda."}
+      />
+
+      {carousels}
+
+      <Footer />
+
     </div>
   );
 }
