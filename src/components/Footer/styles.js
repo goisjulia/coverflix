9add533788910709e84a8d387d9f90796137bd56
Julia Gois <goisjulia@ibm.com>
2020-07-30 02:44:06 -0300

    WIP estilizacao de componentes

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const FooterBase = styled.footer`
   background: var(--black);
-  border-top: 2px solid var(--primary);
   padding-left: 16px;
   padding-right: 16px;
   padding-top: 32px;
