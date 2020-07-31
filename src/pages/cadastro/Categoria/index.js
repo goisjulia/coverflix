51e7c28023215ea67416d65c1a4bf99360f2240f
Julia Gois <goisjulia@ibm.com>
2020-07-31 00:06:28 -0300

    WIP Estilizacao do formulario de categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,29 +1,116 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField/index';
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  width: 50%;
+
+  @media(max-width: 800px){
+    width: 100%;
+  }
+`;
+
+const Button = styled.button`
+  color: var(--black);
+  border: 1px solid var(--black);
+  box-sizing: border-box;
+  cursor: pointer;
+  padding: 5px 10px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  outline: none;
+  border-radius: 5px;
+  text-decoration: none;
+  display: inline-block;
+  transition: opacity .3s;
+
+  &:hover,
+  &:focus {
+    opacity: .5;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  text-align: right;
+`;
 
 function Categoria() {
+
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: ''
+  }
+
+  const [categorias, setCategorias] = useState([]);
+  const [dadosCategoria, setCategoria] = useState(valoresIniciais);
+
+  function setDadosCategoria(chave, valor) {
+    setCategoria({
+      ...dadosCategoria,
+      [chave]: valor
+    })
+  }
+
+  function alterarCategoria(dadoNovo) {
+    setDadosCategoria(
+      dadoNovo.target.getAttribute('name'),
+      dadoNovo.target.value
+    );
+  }
+
   return (
     <>
       <PageDefault>
         <h1> Cadastro de Categoria</h1>
 
-        {/* <form>
-          <label>
-            Nome da Categoria:
-          <input
+        <FormContainer>
+
+          <form onSubmit={function handleSubmit(submit) {
+            submit.preventDefault();
+            setCategorias([
+              ...categorias,
+              dadosCategoria
+            ]);
+            setCategoria(valoresIniciais);
+          }}>
+
+            <FormField
+              label="Nome da categoria"
               type="text"
+              value={dadosCategoria.nome}
+              name="nome"
+              onChange={alterarCategoria}
             />
-          </label>
 
-          <button>
-            Cadastrar
-        </button>
-        </form> */}
+            <FormField
+              label="Descrição"
+              type="textarea"
+              value={dadosCategoria.descricao}
+              name="descricao"
+              onChange={alterarCategoria}
+            />
 
-        <Link to="/">
-          Ir para home
-        </Link>
+            <ul>
+              {categorias.map((categoria, index) => {
+                return (
+                  <li key={`${categoria}${index}`}>
+                    {categoria.nome}
+                  </li>
+                )
+              })}
+            </ul>
+
+            <ButtonContainer>
+              <Button>
+                Cadastrar
+            </Button>
+            </ButtonContainer>
+
+          </form>
+        </FormContainer>
       </PageDefault>
     </>
   );
